87d278cde33496c8f5b7866cc84a6d622bace4ba
windvalley <i@sre.im>
2021-04-04 15:50:58 +0800

    Homebrew: update install command

--- homebrew/brew_cask.sh
+++ homebrew/brew_cask.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
 
+# list apps that already installed
+brew list
 
-# list apps that installed by brew cask
-brew cask list
+# VIM8
+brew install macvim
 
 # iterm2
-brew cask install iterm2
+brew install iterm2
 
 # chrome
-brew cask install google-chrome
+brew install google-chrome
 
 # vscode
-brew cask install visual-studio-code
+brew install visual-studio-code
 
 # postman
-brew cask install postman
+brew install postman
 
 # docker
-brew cask install docker
+brew install docker
 
 # wireshark, need your local root password
-brew cask install wireshark
+brew install wireshark
 
 # typora, the best markdown tool
-brew cask install typora
+brew install typora
 
 # drawio
-brew cask install drawio
-
+brew install drawio
